4b0649852cc6c2296bc47febe154381a91141f31
SolClaude33 <solclaudenft@gmail.com>
2025-10-04 22:12:15 -0300

    Fix mobile responsive issues in hero section
    - Reduce NFT character sizes on mobile to prevent cutoff
    - Adjust positioning to keep all characters visible
    - Optimize logo size for smaller screens (240px max on mobile)
    - Reduce container height on mobile for better fit
    - Adjust cloud positioning to prevent overlap
    - Improve spacing and margins for mobile devices

--- components/hero.tsx
+++ components/hero.tsx
@@ -44,7 +44,7 @@ export function Hero() {
 
       <div className="relative max-w-7xl mx-auto grid lg:grid-cols-2 gap-4 sm:gap-6 md:gap-8 items-end pb-0">
         {/* Left: Logo */}
-        <div className="flex flex-col items-start md:items-start max-md:items-center gap-4 z-10 -mt-20 sm:-mt-32 md:-mt-48 lg:-mt-64 xl:-mt-80 -ml-8 sm:-ml-16 md:-ml-24 lg:-ml-32 xl:-ml-40">
+        <div className="flex flex-col items-start md:items-start max-md:items-center gap-2 sm:gap-4 z-10 -mt-12 sm:-mt-20 md:-mt-32 lg:-mt-48 xl:-mt-64 -ml-4 sm:-ml-8 md:-ml-16 lg:-ml-24 xl:-ml-32">
           <Image
             src="/palu-nft-logo.png"
             alt="PALU NFT wordmark"
@@ -52,7 +52,7 @@ export function Hero() {
             width={640}
             height={400}
             loading="lazy"
-            className="w-full max-w-[280px] sm:max-w-[320px] md:max-w-[400px] lg:max-w-[500px] xl:max-w-[624px] h-auto object-contain hover:scale-105 transition-all duration-500 ease-in-out"
+            className="w-full max-w-[240px] sm:max-w-[280px] md:max-w-[320px] lg:max-w-[400px] xl:max-w-[500px] 2xl:max-w-[624px] h-auto object-contain hover:scale-105 transition-all duration-500 ease-in-out"
             style={{
               filter: "drop-shadow(0 8px 24px rgba(0,0,0,0.18)) drop-shadow(0 0 20px rgba(255,255,255,0.1)) drop-shadow(0 0 40px rgba(59,130,246,0.2))",
               animation: "float 6s ease-in-out infinite",
@@ -61,9 +61,9 @@ export function Hero() {
         </div>
 
         {/* Right: Character scene */}
-        <div className="relative h-[400px] sm:h-[500px] md:h-[600px] lg:h-[700px]">
+        <div className="relative h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] xl:h-[700px]">
           {/* Grey-haired man - left */}
-          <div className="absolute left-[5%] -translate-x-1/2 -bottom-[25%] w-[50%] sm:w-[55%] md:w-[63%] z-20">
+          <div className="absolute left-[8%] sm:left-[6%] md:left-[5%] -translate-x-1/2 -bottom-[25%] w-[40%] sm:w-[50%] md:w-[55%] lg:w-[63%] z-20">
             <Image
               src="/cz-character.png"
               alt="Grey-haired man with glasses"
@@ -78,7 +78,7 @@ export function Hero() {
           </div>
 
           {/* Blond boy - right */}
-          <div className="absolute left-[120%] -translate-x-1/2 -bottom-[35%] w-[60%] sm:w-[65%] md:w-[67.5%] z-15">
+          <div className="absolute left-[100%] sm:left-[110%] md:left-[120%] -translate-x-1/2 -bottom-[35%] w-[50%] sm:w-[60%] md:w-[65%] lg:w-[67.5%] z-15">
             <Image 
               src="/goldieee31.png" 
               alt="Blond boy" 
@@ -93,7 +93,7 @@ export function Hero() {
           </div>
 
           {/* Dog mascot - center */}
-          <div className="absolute left-[62.5%] -translate-x-1/2 -bottom-[25%] w-[70%] sm:w-[80%] md:w-[87%] z-25">
+          <div className="absolute left-[60%] sm:left-[62%] md:left-[62.5%] -translate-x-1/2 -bottom-[25%] w-[60%] sm:w-[70%] md:w-[80%] lg:w-[87%] z-25">
             <Image 
               src="/dog-mascot.png" 
               alt="Dog mascot" 
@@ -108,12 +108,12 @@ export function Hero() {
           </div>
 
           {/* Cloud 1 - bottom left */}
-          <div className="absolute left-[-120%] -bottom-[85%] w-[135%] md:w-[135%] z-30">
+          <div className="absolute left-[-100%] sm:left-[-110%] md:left-[-120%] -bottom-[80%] sm:-bottom-[85%] w-[120%] sm:w-[130%] md:w-[135%] z-30">
             <Image src="/cloud.png" alt="Cloud" width={500} height={200} className="w-full h-auto" />
           </div>
 
           {/* Cloud 2 - bottom left further */}
-          <div className="absolute left-[-175%] -bottom-[95%] w-[135%] md:w-[135%] z-30">
+          <div className="absolute left-[-150%] sm:left-[-165%] md:left-[-175%] -bottom-[90%] sm:-bottom-[95%] w-[120%] sm:w-[130%] md:w-[135%] z-30">
             <Image src="/cloud.png" alt="Cloud" width={500} height={200} className="w-full h-auto" />
           </div>
 
